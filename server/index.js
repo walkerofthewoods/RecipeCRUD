3b6f1e87079db32ffc675fcf1164ffe3623bb1d2
Walker <walkerrm@gmail.com>
2021-02-04 02:15:00 -0500

    submodule removed

--- server/index.js
+++ server/index.js
@@ -2,3 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.listen(bodyParser.json({ limit: '30mb', extended: true }));
+app.listen(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
