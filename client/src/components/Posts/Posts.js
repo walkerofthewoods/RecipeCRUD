be604f8020937891c70e88ba3a8fc290fe7770d7
Walker <walkerrm@gmail.com>
2021-02-07 12:26:48 -0500

    passing state thru parent App

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -4,11 +4,9 @@ import { useSelector } from 'react-redux';
 import Post from './Post/Post';
 import useStyles from './styles';
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
 	const posts = useSelector((state) => state.posts);
 	const classes = useStyles();
-
-	console.log(posts);
 
 	return !posts.length ? (
 		<CircularProgress />
@@ -16,7 +14,7 @@ const Posts = () => {
 		<Grid className={classes.container} container alignItems="stretch" spacing={3}>
 			{posts.map((post) => (
 				<Grid key={post.id} item xs={12} sm={6}>
-					<Post post={post} />
+					<Post post={post} setCurrentId={setCurrentId} />
 				</Grid>
 			))}
 		</Grid>
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,10 +1,10 @@
+import pkg from 'mongoose';
+const { Mongoose } = pkg;
 import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async (req, res) => {
 	try {
 		const postMessages = await PostMessage.find();
-
-		console.log(postMessages);
 
 		res.status(200).json(postMessages);
 	} catch (error) {
@@ -24,4 +24,15 @@ export const createPost = async (req, res) => {
 	} catch (error) {
 		res.status(409).json({ message: error.message });
 	}
+};
+
+export const updatePost = async (req, res) => {
+	const { id: _id } = req.params;
+	const post = req.body;
+
+	if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+	const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+
+	res.json(updatePost);
 };
